2130ff4f1bdc5fb9a9669b3bee1e7d027b355c8e
Mateo Presa Castro <mateopresacastro@gmail.com>
2024-12-02 09:14:17 +0100

    Day 2 part 1

---
+++ 2024/deno/2/1a.ts
@@ -0,0 +1,35 @@
+import { TextLineStream } from "jsr:@std/streams@0.224.0/text-line-stream";
+
+using f = await Deno.open("input.txt");
+const lines = f.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+
+let safeReports = 0;
+
+for await (const line of lines) {
+    const parsedLine = parse(line);
+    if (isSafe(parsedLine)) safeReports++;
+}
+
+console.log(safeReports);
+
+function parse(line: string) {
+    return line.split(" ").map(Number);
+}
+
+function isSafe(line: number[]) {
+    if (line.length <= 1) return false;
+    const set = new Set(line);
+    if (set.size !== line.length) return false;
+    const sorted = line.toSorted((a, b) => b - a);
+    const sortedStr = sorted.join("");
+    const revStr = sorted.toReversed().join("");
+    const lineStr = line.join("");
+    if (lineStr !== sortedStr && lineStr !== revStr) return false;
+    for (let i = 1; i < line.length; i++) {
+        if (Math.abs(line.at(i)! - line.at(i - 1)!) > 3) return false;
+    }
+
+    return true;
+}
